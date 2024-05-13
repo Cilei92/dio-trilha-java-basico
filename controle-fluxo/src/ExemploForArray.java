5fe478b6f8b98039019939bbd5108c44df238d90
Cilei92 <cilei_duarte@hotmail.com>
2024-05-13 10:28:18 -0400

    corrigindo problema

--- controle-fluxo/src/ExemploForArray.java
+++ controle-fluxo/src/ExemploForArray.java
@@ -2,8 +2,8 @@ public class ExemploForArray {
  public static void main(String[] args) {
     String alunos[] = {"Felipe", "Jonas", "Julia", "Marcos" };
 
-    for (int x=0; x<alunos.length; x++){
-        System.out.println("O aluno no indecex= " + x + "é " + alunos[x]);
+    for (int x=1; x<alunos.length; x++){
+        System.out.println("O aluno no indecex= " + x + " é " + alunos[x]);
     }
  }
 }
